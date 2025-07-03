f161175de9a29a2bee1cf00e6d9d43402da52371
Kaike <kaikedias98@gmail.com>
2025-07-03 15:21:55 -0300

    feat: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <div className="flex justify-center py-2 font-semibold text-white bg-[#219ebc] shadow-md">
+      <h1>Dieta Express</h1>
+    </div>
+  );
+}
